42c770d8814fbc1b187965d4211c1600a6545d7c
Greg Linton <lintong@pagodabox.com>
2015-08-03 12:57:09 -0400

    Incorporate boot2docker box, link against nanobox binary, make the man page manly.

--- linux/build-deb.sh
+++ linux/build-deb.sh
@@ -1,15 +1,25 @@
 mkdir -p nanobox/DEBIAN
-mkdir -p nanobox/usr/local/bin
+mkdir -p nanobox/opt/nanobox/bin
+mkdir -p nanobox/opt/nanobox/share
 mkdir -p nanobox/usr/share/man/man1
 mkdir -p nanobox/usr/share/doc/nanobox
 
-wget -O nanobox/usr/local/bin/nanobox https://s3.amazonaws.com/tools.nanobox.io/cli/linux/amd64/nanobox
-chmod 755 nanobox/usr/local/bin/nanobox
+NANOBOX_VERS="0.0.7"
+BOOT2DOCKER_VERS="0.0.7"
+
+if ! [ -x nanobox/opt/nanobox/bin/nanobox ]; then
+  wget -O nanobox/opt/nanobox/bin/nanobox https://s3.amazonaws.com/tools.nanobox.io/cli/linux/amd64/nanobox
+  chmod 755 nanobox/opt/nanobox/bin/nanobox
+fi
+
+if ! [ -a nanobox/opt/nanobox/share/nanobox-boot2docker.box ]; then
+  wget -O nanobox/opt/nanobox/share/nanobox-boot2docker.box https://github.com/pagodabox/nanobox-boot2docker/releases/download/v$(BOOT2DOCKER_VERS)/nanobox-boot2docker.box
+fi
 
 # create control file
-cat > debian/DEBIAN/control <<-EOF
+cat > nanobox/DEBIAN/control <<-EOF
 Package: nanobox
-Version: 0.0.7
+Version: $NANOBOX_VERS
 Section: base
 Priority: optional
 Architecture: all
@@ -20,30 +30,37 @@ Description: Nanobox cli utility
 EOF
 
 # create postinstall file (for docs?)
-cat > nanobox/nanobox/postinst <<-'EOF'
-#!/bin/sh
-set -e
+cat > nanobox/DEBIAN/postinst <<-'EOF'
+#!/bin/sh -e
 # Automatically added by dh_installdocs
 if [ "$1" = "configure" ]; then
   if [ -d /usr/doc -a ! -e /usr/doc/nanobox -a -d /usr/share/doc/nanobox ]; then
     ln -sf ../share/doc/nanobox /usr/doc/nanobox
   fi
-fi
 # End automatically added section
+
+  ln -sf /opt/nanobox/bin/nanobox /usr/local/bin/nanobox
+  which vagrant > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    vagrant box add --force --name nanobox/boot2docker /opt/nanobox/share/nanobox-boot2docker.box > /dev/null 2>&1
+  fi
+fi
 EOF
 
 # create pre-removal file (for docs?)
-cat > nanobox/nanobox/prerm <<-'EOF'
-#!/bin/sh
-set -e
-# Automatically added by dh_installdocs
-if [ \( "$1" = "upgrade" -o "$1" = "remove" \) -a -L /usr/doc/nanobox ]; then
-  rm -f /usr/doc/nanobox
+cat > nanobox/DEBIAN/prerm <<-'EOF'
+#!/bin/sh -e
+if [ "$1" = "upgrade" -o "$1" = "remove" ]; then
+  rm -f /usr/local/bin/nanobox
+  vagrant box list | grep '^nanobox/boot2docker ' && vagrant box remove nanobox/boot2docker --force > /dev/null 2>&1 || true
+
+  if [ -L /usr/doc/nanobox ]; then
+    rm -f /usr/doc/nanobox
+  fi
 fi
-# End automatically added section
 EOF
 
-chmod 755 nanobox/nanobox/p*
+chmod 755 nanobox/DEBIAN/p*
 
 # create copyright file
 cat > nanobox/usr/share/doc/nanobox/copyright <<-EOF
@@ -56,14 +73,17 @@ Mozilla Public License, version 2.0
 EOF
 
 # create changelog file
-cat > nanobox/usr/share/doc/nanobox/changelog <<-EOF
-nanobox 0.0.7
+cat >> nanobox/usr/share/doc/nanobox/changelog <<-EOF
+nanobox $NANOBOX_VERS
   * Created package
  -- Pagoda Box 2015
 EOF
 
 # create manpage
 cat > nanobox/usr/share/man/man1/nanobox.1 <<-EOF
+.TH NANOBOX 1 "`date +"%d %b %Y"`" "$NANOBOX_VERS"
+.SH NAME
+nanobox \- nanobox command line utility
 
                                      ***
                                   *********
@@ -74,61 +94,59 @@ cat > nanobox/usr/share/man/man1/nanobox.1 <<-EOF
                              ...     ***     ...
                          +++      ...   ...      +++
                              +++     ...     +++
-                         \\\      +++   +++      ///
-                             \\\     +++     ///
-                                  \\     //
-                                     \//
+                         \\\\\\      +++   +++      ///
+                             \\\\\\     +++     ///
+                                  \\\\     //
+                                     \\//
 
                       _  _ ____ _  _ ____ ___  ____ _  _
-                      |\ | |__| |\ | |  | |__) |  |  \/
-                      | \| |  | | \| |__| |__) |__| _/\_
+                      |\\ | |__| |\\ | |  | |__) |  |  \\/
+                      | \\| |  | | \\| |__| |__) |__| _/\\_
 
 
+.SH SYNOPSIS
+nanobox <COMMAND> [--debug]
 
-Description:
-  Welcome to the nanobox CLI! This will be your primary tool when working with
-  nanobox. If you encounter any issues or have any suggestions, find us on
-  IRC (freenode) at #nanobox. Our engineers are available between 8 - 5pm MST.
+.SH DESCRIPTION
+Welcome to the nanobox CLI! This will be your primary tool when working with
+nanobox. If you encounter any issues or have any suggestions, find us on
+IRC (freenode) at #nanobox. Our engineers are available between 8 - 5pm MST.
 
-  All commands have a short [-*] and a verbose [--*] option when passing flags.
+All commands have a short [-*] and a verbose [--*] option when passing flags.
 
-  You can pass -h, --help, or help to any command to receive detailed information
-  about that command.
+You can pass -h, --help, or help to any command to receive detailed information
+about that command.
 
-  You can pass --debug at the end of any command to see all request/response
-  output when making API calls.
+You can pass --debug at the end of any command to see all request/response
+output when making API calls.
 
-Usage:
-  pagoda (<COMMAND>:<ACTION> OR <ALIAS>) [GLOBAL FLAG] <POSITIONAL> [SUB FLAGS] [--debug]
+.SH OPTIONS
+-h, --help, help
+  Run anytime to receive detailed information about a command.
 
-Options:
-  -h, --help, help
-    Run anytime to receive detailed information about a command.
+-v, --version, version
+  Run anytime to see the current version of the CLI.
 
-  -v, --version, version
-    Run anytime to see the current version of the CLI.
+--debug
+  Shows all API request/response output. MUST APPEAR LAST
 
-  --debug
-    Shows all API request/response output. MUST APPEAR LAST
+.SH COMMANDS
+.IP "create      : Runs an 'init' and starts a nanobox VM"
+.IP "deploy      : Deploys to your nanobox VM"
+.IP "destroy     : Destroys the current nanobox VM"
+.IP "halt        : Halts the current nanobox VM"
+.IP "help        : Display this help"
+.IP "init        : Creates a nanobox flavored Vagrantfile"
+.IP "log         : Show/Stream nanobox logs"
+.IP "new         : Create a new nanobox developer project"
+.IP "publish     : Publish your nanobox live"
+.IP "reload      :"
+.IP "resume      : Resumes a halted/suspened nanobox VM"
+.IP "status      : Display all current nanobox VM's"
+.IP "suspend     : Suspends the current nanobox VM"
+.IP "up          : Runs a 'create' and a 'deploy'"
+.IP "update      : Updates this CLI to the newest version"
 
-Available Commands:
-
-  create      : Runs an 'init' and starts a nanobox VM
-  deploy      : Deploys to your nanobox VM
-  destroy     : Destroys the current nanobox VM
-  halt        : Halts the current nanobox VM
-  help        : Display this help
-  init        : Creates a nanobox flavored Vagrantfile
-  log         : Show/Stream nanobox logs
-  new         : Create a new nanobox developer project
-  publish     : Publish your nanobox live
-  reload      :
-  resume      : Resumes a halted/suspened nanobox VM
-  status      : Display all current nanobox VM's
-  suspend     : Suspends the current nanobox VM
-  up          : Runs a 'create' and a 'deploy'
-  update      : Updates this CLI to the newest version
-  
 EOF
 
 # gzip manpage
@@ -136,4 +154,4 @@ gzip --best nanobox/usr/share/man/man1/nanobox.1
 # build package
 dpkg-deb --build nanobox
 # rename package
-mv nanobox.deb nanobox-0.0.7.deb
+mv nanobox.deb nanobox-$(NANOBOX_VERS).deb
