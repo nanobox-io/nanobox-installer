9a0db8b99915054342e78df859faa102253b5296
Greg Linton <lintong@pagodabox.com>
2015-11-24 17:02:56 -0700

    Trying different ways to install all nanobox packages without the install process breaking

--- linux/build-deb-all.sh
+++ linux/build-deb-all.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
-mkdir -p nanobox-bundle/opt/nanobox/share
 
 # gzip docs
 [ -f nanobox-bundle/usr/share/doc/nanobox-bundle/changelog ] && gzip -n --best nanobox-bundle/usr/share/doc/nanobox-bundle/changelog || true
 
 # proper permissions
-chmod 644 nanobox-bundle/opt/nanobox/share/virtualbox.deb
-chmod 644 nanobox-bundle/opt/nanobox/share/vagrant.deb
-
 chmod 644 nanobox-bundle/usr/share/doc/nanobox-bundle/changelog.gz
 chmod 644 nanobox-bundle/usr/share/doc/nanobox-bundle/copyright
 
